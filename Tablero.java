a3c77c915a8996066e90e028634e561fc5d77b7f
Aldrich <aldrich.lay@gmail.com>
2019-09-25 10:07:57 -0300

    Remplazo 1 Tablero

--- Tablero.java
+++ Tablero.java
@@ -2,5 +2,23 @@
 package ajedrez;
 
 public class Tablero {
-    
+    public char[][] Tablero;
+
+    public void crear(){
+        Tablero = new char[7][7];
+        
+        for(int i = 0; i < 7; i++){
+            for(int j = 0; j < 7; j++){
+                Tablero[i][j] = '|';
+            }
+        }
+    }
+    public void mostrar(){
+        for(int i=0; i < 7; i++){
+            for(int j=0; j < 7; j++){
+                System.out.print(Tablero[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+    }        
 }
